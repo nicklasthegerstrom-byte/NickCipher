3a781f0a52d9058139042f6900adda1e8c55d1ea
Nicklas Thegerström <nicklas.thegerstrom@gmail.com>
2025-12-20 20:12:05 +0100

    first menu added

---
+++ nickcipher/ui/ui.py
@@ -0,0 +1,12 @@
+
+
+def print_menu():
+    print("Welcome to NickCipher – your friendly text to emoji encryption tool.")
+    print()
+    print("1. Encrypt text (input)")
+    print("2. Decrypt text (input)")
+    print("3. Encrypt from file")
+    print("4. Information")
+    print("5. Exit")
+
+def app():
