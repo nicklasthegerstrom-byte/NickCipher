353a79e195d60a4f90b4c57654276322f6e8d9dd
Nicklas Thegerström <nicklas.thegerstrom@gmail.com>
2025-12-22 23:10:58 +0100

    start of UI

--- nickcipher/ui/ui.py
+++ nickcipher/ui/ui.py
@@ -1,4 +1,4 @@
-
+from nickcipher.core.cipher import DynamicEmojiCipher
 
 def print_menu():
     print("Welcome to NickCipher – your friendly text to emoji encryption tool.")
@@ -6,7 +6,20 @@ def print_menu():
     print("1. Encrypt text (input)")
     print("2. Decrypt text (input)")
     print("3. Encrypt from file")
-    print("4. Information")
-    print("5. Exit")
+    print("4. Decrypt from file")
+    print("5. Information")
+    print("6. Save/load encryption key")
+    print("7. Exit")
 
 def app():
+    
+    while True:
+        print_menu()
+
+        choice = input("Make your choice")
+
+        if choice == 1:
+            password_input = input("Choose password")
+            text_input = input("Input your text to encrypt here:")
+            
+
