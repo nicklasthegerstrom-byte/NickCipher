ce3cba67df321ce6d80a44e82f5e9c1e8a83f3c8
Nicklas Thegerström <nicklas.thegerstrom@gmail.com>
2025-12-22 15:21:26 +0100

    testar main

--- nickcipher/main.py
+++ nickcipher/main.py
@@ -13,7 +13,7 @@ weights = load_weights(weights_path)
 cipher = DynamicEmojiCipher(emoji_pool, weights)
 cipher.generate_key(12345)
 
-text = "Hej här är text som jag bara vill testa för att se om kryptering funkar"
+text = "I en värld som ständigt förändras har tekniken blivit den osynliga tråden som binder samman oss alla. För bara några decennier sedan var tanken på en global superdator i fickan inget annat än ren science fiction, men idag är det vår verklighet. Vi navigerar genom digitala landskap med en hastighet som våra förfäder aldrig hade kunnat föreställa sig."
 
 encoded_text = cipher.encode(text.lower())
 
